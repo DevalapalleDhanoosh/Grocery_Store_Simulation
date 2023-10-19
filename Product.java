999d59615a970ed1ae982c239a921bb88d061be5
Dhanoosh-12214433 <111235548+DevalapalleDhanoosh@users.noreply.github.com>
2023-10-19 15:32:43 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,15 @@
+class Product {
+    String name;
+    double price;
+    int quantity;
+    String category;
+    String unit;
+
+    public Product(String name, double price, int quantity, String category, String unit) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.category = category;
+        this.unit = unit;
+    }
+}
